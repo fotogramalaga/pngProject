b9c77c1999313766c0908a5c3b180e0d6bf51117
Lfrty <matiasdelosriosaguilar@gmail.com>
2022-03-18 14:07:03 +0100

    login works!

--- pngProject/src/app/app.routing.module.ts
+++ pngProject/src/app/app.routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ImagenesComponent } from './imagenes/imagenes.component';
 
 import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'imagenes', component: ImagenesComponent },
 ];
 
 @NgModule({
